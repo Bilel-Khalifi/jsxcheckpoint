f0649a4447593370dc2e7c5d3db214a1064725c3
Bilel-Khalifi <khalifi.bilel@gmail.com>
2021-10-24 17:16:19 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import myimage from './imageInSrc.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <div style={{border:'solid 1px black', maxWidth:"100vw"}}>
+
+      <h1 className="title red">Bilel Khlifi</h1>
+
+      <br/>
+
+      <img src={myimage}  alt="" />
+
+      <br/>
+
+      <img src="/imageInPublic.jpg" alt="" />
+
+      </div>
+
+<video
+  id="footage_291"
+  className="video-js"
+  width={320}
+  height={240}
+  controls
+  poster="https://images.all-free-download.com/images/footage_large/tree_291.jpg"
+  loop
+>
+  <source
+    src="https://images.all-free-download.com/footage_preview/mp4/tree_291.mp4"
+    type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+  />
+  <source
+    src="https://images.all-free-download.com/footage_preview/webm/tree_291.webm"
+    type='video/webm; codecs="vp8, vorbis"'
+  />
+  <source
+    src="https://images.all-free-download.com/footage_preview/ogv/tree_291.ogv"
+    type='video/ogg; codecs="theora, vorbis"'
+  />
+  <img
+    src="https://images.all-free-download.com/images/footage_large/tree_291.jpg"
+    width={425}
+    height={240}
+    alt="Poster Image"
+    title="No video playback capabilities."
+  />
+
+
+      </video>
+      </div>
   );
 }
 
